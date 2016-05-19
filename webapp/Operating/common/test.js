ef3bcafe868069eff154aad20836e21450357c25
xiaolibier <xiaolibier@126.com>
2016-05-19 15:42:36 +0800

    no message

--- webapp/Operating/common/test.js
+++ webapp/Operating/common/test.js
@@ -28,16 +28,19 @@ $(document).ready(function(){
 					timestamp:timestamp, // 必填，生成签名的时间戳
 					nonceStr: nonceStr, // 必填，生成签名的随机串
 					signature: signature,// 必填，签名，见附录1
-					jsApiList: ['ready','error','onMenuShareTimeline','onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+					jsApiList: ['ready','error','showOptionMenu','onMenuShareTimeline','onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 				});
+				wx.showOptionMenu();
+
 				wx.ready(function(){	
 					// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
 				});
 				wx.error(function(res){	
 					// config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
 				});
+				/* 分享到朋友圈 */
 				wx.onMenuShareTimeline({
-					title: '测试分享1', // 分享标题
+					title: '燕子安家，5.20分期享好礼，分享献爱心~', // 分享标题
 					link: 'http://www.yanzianjia.com', // 分享链接
 					imgUrl: 'http://www.yanzianjia.com', // 分享图标
 					success: function () { 
@@ -49,6 +52,7 @@ $(document).ready(function(){
 						// 用户取消分享后执行的回调函数
 					}
 				});	
+				/* 发送给朋友 */
 				wx.onMenuShareAppMessage({
 					title: '测试分享2', // 分享标题
 					desc: '11', // 分享描述
@@ -64,78 +68,43 @@ $(document).ready(function(){
 						alert('分享回调失败');
 						// 用户取消分享后执行的回调函数
 					}
-				}); 		
+				}); 
+				/* 分享到qq */
+				wx.onMenuShareQQ({
+					title: '', // 分享标题
+					desc: '', // 分享描述
+					link: '', // 分享链接
+					imgUrl: '', // 分享图标
+					success: function () { 
+					   // 用户确认分享后执行的回调函数
+					},
+					cancel: function () { 
+					   // 用户取消分享后执行的回调函数
+					}
+				});
+				/* 分享到微博 */
+				wx.onMenuShareWeibo({
+					title: '', // 分享标题
+					desc: '', // 分享描述
+					link: '', // 分享链接
+					imgUrl: '', // 分享图标
+					success: function () { 
+					   // 用户确认分享后执行的回调函数
+					},
+					cancel: function () { 
+						// 用户取消分享后执行的回调函数
+					}
+				});
+				
 			},
 			error:function(data){
 			}
 		});
 		
 	}
-	window.appId = function(appId,timestamp,nonceStr,signature){
-		console.log(appId);
-		console.log(timestamp);
-		console.log(nonceStr);
-		console.log(signature);
-		wx.config({
-			debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-			appId: appId, // 必填，公众号的唯一标识
-			timestamp:timestamp, // 必填，生成签名的时间戳
-			nonceStr: nonceStr, // 必填，生成签名的随机串
-			signature: signature,// 必填，签名，见附录1
-			jsApiList: ['ready','error','onMenuShareTimeline','onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-		});
-		wx.ready(function(){
-			//alert('配置成功');
-			wx.onMenuShareTimeline({
-				title: '测试分享', // 分享标题
-				link: 'http://www.yanzianjia.com', // 分享链接
-				imgUrl: 'http://www.yanzianjia.com', // 分享图标
-				success: function () { 
-					alert('分享成功');
-					// 用户确认分享后执行的回调函数
-				},
-				cancel: function () { 
-					alert('分享失败');
-					// 用户取消分享后执行的回调函数
-				}
-			});	
-			wx.onMenuShareAppMessage({
-				title: '测试分享2', // 分享标题
-				desc: '11', // 分享描述
-				link: '1', // 分享链接
-				imgUrl: '2', // 分享图标
-				type: '', // 分享类型,music、video或link，不填默认为link
-				dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-				success: function () { 
-					alert('分享回调成功');
-					// 用户确认分享后执行的回调函数
-				},
-				cancel: function () { 
-					alert('分享回调失败');
-					// 用户取消分享后执行的回调函数
-				}
-			}); 
-			// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-		});
-		wx.error(function(res){
-			alert('配置失败');
-			// config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+	var myCity = new BMap.LocalCity();
+		myCity.get(myFun);
 
-		});
-		/* wx.checkJsApi({
-			jsApiList: ['onMenuShareTimeline'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
-			success: function(res) {
-				//alert(res);
-				// 以键值对的形式返回，可用的api值true，不可用为false
-				// 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-			}
-		});
-		*/
-	}
-	//var myCity = new BMap.LocalCity();
-		//myCity.get(myFun);
-		
-	
 	g.page_now = Utils.offLineStore.get("nowPage",false) || "1";
 	g.userCity = Utils.offLineStore.get("userCity",false) || "请选择";
 	g.operate = Utils.getQueryString("O") || "";//获取运营人员
