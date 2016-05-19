7c64b860ff5cc849b039c78c1cd705881583e08f
xiaolibier <xiaolibier@126.com>
2016-05-19 16:01:44 +0800

    no message

--- webapp/Operating/common/test.js
+++ webapp/Operating/common/test.js
@@ -31,7 +31,15 @@ $(document).ready(function(){
 					jsApiList: ['ready','error','showAllNonBaseMenuItem','onMenuShareTimeline','onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 				});
 				
-
+				wx.checkJsApi({
+				  jsApiList: [
+					'showOptionMenu',
+					'onMenuShareTimeline'
+				  ],
+				  success: function (res) {
+					alert(res);
+				  }
+				});
 				wx.ready(function(){
 					wx.showOptionMenu();
 					wx.showMenuItems({
@@ -278,13 +286,6 @@ $(document).ready(function(){
 	}
 	
 	function submit_form(){
-		wx.ready(function(){
-					wx.showOptionMenu();
-					wx.showMenuItems({
-						menuList: ['menuItem:openWithSafari','menuItem:refresh'] // 要显示的菜单项，所有menu项见附录3
-					});
-					// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-				});
 		var condi = {};
 		condi.userCity = $("#userCity").val() || "";
 		condi.userName = $("#userName").val() || "";
