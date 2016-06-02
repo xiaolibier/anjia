468fb549e83697c24eaad73fc11658aace6ece47
xiaolibier <xiaolibier@126.com>
2016-06-02 16:56:15 +0800

    no message

--- ajadmin/Public/js/login.js
+++ ajadmin/Public/js/login.js
@@ -77,7 +77,7 @@ $(function () {
         $.ajax({
             url: url, data: condi, type: "POST", dataType: "json", context: this,
             success: function (data) {
-                console.log("sendLoginHttp",data);
+                //console.log("sendLoginHttp",data);
                 var status = data.success || false;
                 if (status) {
                     var userInfo = data.obj || "";
@@ -106,6 +106,7 @@ $(function () {
                 //alert(XMLHttpRequest.status);
                 //alert(XMLHttpRequest.readyState);
                 //alert(textStatus);
+            	alert('网络连接错误,请联系管理员.');
             }
         });
     }
