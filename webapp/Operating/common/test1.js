222ef0cb660b335622eab6292eb61b4080c24710
xiaolibier <xiaolibier@126.com>
2016-06-06 15:06:00 +0800

    no message

--- webapp/Operating/common/test1.js
+++ webapp/Operating/common/test1.js
@@ -73,7 +73,7 @@ $(document).ready(function(){
 						   "paySign" : paySign //微信签名 
 					   },
 					   function(res){
-						  //alert(res);
+						  alert(res.err_msg);
 						  if(res.err_msg == "get_brand_wcpay_request：ok" ) {alert('成功')}
 						  else if(res.err_msg == "get_brand_wcpay_request：cancel"){alert('失败')}
 						  else if(res.err_msg == "get_brand_wcpay_request：fail"){alert('失败')}// 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
