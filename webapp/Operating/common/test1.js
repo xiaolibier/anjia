d3972f618a56b236023239f4d5ca47fce4b173a1
xiaolibier <xiaolibier@126.com>
2016-06-06 16:51:58 +0800

    no message

--- webapp/Operating/common/test1.js
+++ webapp/Operating/common/test1.js
@@ -73,7 +73,7 @@ $(document).ready(function(){
 					jsApiList: ['onMenuShareQZone','onMenuShareWeibo','onMenuShareQQ','onMenuShareTimeline','onMenuShareAppMessage','chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 				});
 				 wx.chooseWXPay({
-							timestamp: 0, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+							timestamp: g.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
 							nonceStr: g.nonceStr, // 支付签名随机串，不长于 32 位
 							package: g.packAge, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
 							signType: g.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
