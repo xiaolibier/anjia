28cb726cda682e84a898ae9c5578f6bae6615cc0
xiaolibier <xiaolibier@126.com>
2016-06-06 17:15:34 +0800

    no message

--- webapp/Operating/common/test1.js
+++ webapp/Operating/common/test1.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
 	/* 测试获取分享 */	
 	var g = {};
-	onBridgeReady();
+
 	
 	var myCity = new BMap.LocalCity();
 		myCity.get(myFun);
@@ -72,13 +72,33 @@ $(document).ready(function(){
 					signature: signature,// 必填，签名，见附录1
 					jsApiList: ['onMenuShareQZone','onMenuShareWeibo','onMenuShareQQ','onMenuShareTimeline','onMenuShareAppMessage','chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 				});
-				alert('timestamp:'+g.timestamp+";nonceStr:"+g.nonceStr+";package:"+g.packAge+";signType:"+g.signType+";paySign:"+g.paySign);
+				var url = Base.serverUrl + "weixin/pay/getBrandWCPayConfig";
+		$.ajax({
+			url:url,
+			data:'',
+			type:"POST",
+			dataType:"json",
+			context:this,
+			global:false,
+			success: function(data){
+				var success = data.success || true;
+				if(success){
+					 /* -- */
+					 var d = data.obj || {};
+					 var timeStamp = d.timeStamp || "";
+					 var packAge = d.packAge || "";
+					 var paySign = d.paySign || "";
+					 var appId = d.appId || "";
+					 var signType = d.signType || "";
+					 var nonceStr = d.nonceStr || "";
+					 
+					 alert('timestamp:'+timestamp+";nonceStr:"+nonceStr+";package:"+packAge+";signType:"+signType+";paySign:"+paySign);
 				 wx.chooseWXPay({
-							timestamp: g.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-							nonceStr: g.nonceStr, // 支付签名随机串，不长于 32 位
-							package: g.packAge, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-							signType: g.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-							paySign: g.paySign, // 支付签名
+							timestamp: timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+							nonceStr: nonceStr, // 支付签名随机串，不长于 32 位
+							package: packAge, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+							signType: signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+							paySign: paySign, // 支付签名
 							success: function (res) {
 								// 支付成功后的回调函数
 								alert(res);
@@ -87,6 +107,16 @@ $(document).ready(function(){
 								alert(res);
 							}
 						});
+				}
+				else{
+					var msg = data.message || "获取失败";
+					alert(msg);
+				}
+			},
+			error:function(data){
+			}
+		});
+				
 				/* 分享到朋友圈 */
 				wx.onMenuShareTimeline({
 					title: '燕子安家，5.20分期享好礼，分享献爱心', // 分享标题
