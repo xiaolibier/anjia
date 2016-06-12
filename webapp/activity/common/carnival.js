6f5f28dd2c9cde7ef9560634237294307c089ad7
xiaolibier <xiaolibier@126.com>
2016-06-12 18:05:02 +0800

    12

--- webapp/activity/common/carnival.js
+++ webapp/activity/common/carnival.js
@@ -185,23 +185,20 @@ $(document).ready(function(){
 					 var signType = d.signType || "";
 					 var nonceStr = d.nonceStr || "";
 					 
-					 WeixinJSBridge.invoke(
-					   'getBrandWCPayRequest', {
-						   "appId" : appId, //公众号名称，由商户传入     
-						   "timeStamp":timeStamp, //时间戳，自1970年以来的秒数     
-						   "nonceStr" : nonceStr, //随机串     
-						   "package" : packAge,
-						   "signType" : signType,//微信签名方式：     
-						   "paySign" : paySign //微信签名 
-					   },
-					   function(res){
-						 // alert(res.err_msg);
-						  /* if(res.err_msg == "get_brand_wcpay_request：ok" ) {alert('成功')}
-						  else if(res.err_msg == "get_brand_wcpay_request：cancel"){alert('失败')}
-						  else if(res.err_msg == "get_brand_wcpay_request：fail"){alert('失败')} */// 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
-					   }
-				   );
-					/* -- */
+					wx.chooseWXPay({
+							timestamp: timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+							nonceStr: nonceStr, // 支付签名随机串，不长于 32 位
+							package: packAge, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+							signType: signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+							paySign: paySign, // 支付签名
+							success: function (res) {
+								// 支付成功后的回调函数
+								//alert(res);
+							},
+							cancel: function (res) { 
+								//alert(res);
+							}
+						});
 				}
 				else{
 					var msg = data.message || "获取失败";
