c9403e9e68db2717fc8f1a1f66c6a426de47da09
xiaolibier <xiaolibier@126.com>
2016-06-13 10:25:19 +0800

    123

--- anjia/js/carnival.js
+++ anjia/js/carnival.js
@@ -18,11 +18,9 @@ $(document).ready(function(){
 	function erWeiMa_func(){
 		if(g.customerCollectId != ""){
 			$("#img_span").empty();
-			var str = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx8e885de9bab84c4d&redirect_uri=http://m.yanzianjia.com/webapp/activity/?cus='+g.customerCollectId+'&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect';
+			var str = 'http://m.yanzianjia.com/weixin/authorize?redirect_uri=http://m.yanzianjia.com/webapp/activity/?cus='+g.customerCollectId+"";
 			$("#img_span").qrcode({
 				render: "table",
-				
-				
 				text: str
 			});
 		}
