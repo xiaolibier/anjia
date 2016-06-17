59d07176be3c4938b5721ac8037fc62584150b99
xiaolibier <xiaolibier@126.com>
2016-06-17 16:23:20 +0800

    1

--- webapp/homeTown/common/js/loding.js
+++ webapp/homeTown/common/js/loding.js
@@ -1,17 +1,59 @@
 $(function(){
 	var g = {};
-	
+	g.zan = 0;//点赞数
+	g.fangwen = 0;
 	
 	loding();
 	read_txt();
 	read_database();
 	
-	/* 存取读取数据 */
-	function write_database(){
+	$('.mod-nav.no-icon').bind('click',write_zan);
+	/* 写入点赞 */
+	function write_zan(){
 		var condi = {};
-		condi.activityId = "789456321";
-		condi.userPhone = "13520478359";
-		var url = Base.serverUrl + "questionnaire/queryQuestionnaires";
+		condi.channel = "789456321";
+		condi.userPhone = getNum();
+		condi.question1 = g.fangwen;
+		condi.question2 = parseInt(g.zan)+1;
+		var url = Base.serverUrl + "questionnaire/gydcwj";
+		$.ajax({
+			url:url,
+			data:condi,
+			type:"POST",
+			dataType:"json",
+			context:this,
+			success : function(data){
+				var status = data.success || false;
+				if(status){
+					
+				}
+				else{
+					var msg = data.message || "获取失败";
+					Utils.alert(msg);
+				}
+				
+			},
+			error:function(data){
+			}
+		});
+	}
+	/* 获取日期时间 */
+	function getNum(){
+		var Num=""; 
+		for(var i=0;i<11;i++) 
+		{ 
+		Num+=Math.floor(Math.random()*10); 
+		}
+		return Num;
+	}
+	/* 写入访问量 */
+	function write_database(num){
+		var condi = {};
+		condi.channel = "789456321";
+		condi.userPhone = getNum();
+		condi.question1 = num;
+		condi.question2 = parseInt(g.zan);
+		var url = Base.serverUrl + "questionnaire/gydcwj";
 		$.ajax({
 			url:url,
 			data:condi,
@@ -33,21 +75,34 @@ $(function(){
 			}
 		});
 	}	
-	
+	/* 读取访问量 */
 	function read_database(){
 		var condi = {};
-		condi.activityId = "1";
-		condi.question1 = 1;
+		condi.channel = "789456321";
+		condi.activityId = '1'
 		var url = Base.serverUrl + "questionnaire/queryQuestionnaires";
 		$.ajax({
 			url:url,
 			data:condi,
 			type:"POST",
-			dataType:"text",
+			dataType:"json",
 			context:this,
-			success : function(d){
-				
-				
+			success : function(data){
+				var success = data.success || false;
+				if(success){
+					var d = data.obj || [];
+					var num = d[0].question1 || 0;//记录访问量
+					var num2 = d[0].question2 || 1;//记录点赞数
+					$('.count_user_get').html(num);
+					$('.count_zan_num').html(num2);
+					g.zan = num2;
+					num = parseInt(num)+1;
+					g.fangwen = num;
+					write_database(num);
+				}else{
+					var msg = data.message || "失败";
+					Utils.alert(msg);
+				}
 			},
 			error:function(data){
 			}
