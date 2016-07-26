db27980f8c6518b7057bb2b6621c9248bdbea8ae
xiaolibier <xiaolibier@126.com>
2016-07-26 11:58:49 +0800

    测试

--- admin/Order/js/dashboard.js
+++ admin/Order/js/dashboard.js
@@ -181,7 +181,7 @@ $(function(){
 					html+= '</tr>';
 					for(var i=0,len=dw.length;i<len;i++){
 						var city = dw[i].city || "";
-						var provinceCode = dw[i].provinceCode || "";
+						var provinceCode = dw[i].provinceCode+"" || "";
 						//var p = eval(provinceCode) || {};
 						var color = i > 3 ? 'ffac89' : 'ff773e';
 						if(i == 0)color = 'ff4c00';
@@ -190,7 +190,7 @@ $(function(){
 						html+= '</tr>';
 						//console.log(chinaMapConfig.names.p);
 						console.log(chinaMapConfig['names'][provinceCode]);
-						chinaMapConfig['names']['yunnan'].color=color;
+						chinaMapConfig['names'][provinceCode].color=color;
 						
 					}
 					$("#city_list").html(html);
