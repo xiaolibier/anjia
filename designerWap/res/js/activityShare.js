2b8cded9f521ffa65e5e1adeb0f486da3f2b5026
xiaolibier <xiaolibier@126.com>
2016-08-01 16:11:33 +0800

    11

--- designerWap/res/js/activityShare.js
+++ designerWap/res/js/activityShare.js
@@ -97,7 +97,7 @@ $(function(){
 				var timestamp = d.timestamp || "";
 				
 				wx.config({
-					debug: '', // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+					debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 					appId: appId, // 必填，公众号的唯一标识
 					timestamp:timestamp, // 必填，生成签名的时间戳
 					nonceStr: nonceStr, // 必填，生成签名的随机串
@@ -111,10 +111,12 @@ $(function(){
 						imgUrl: imgUrl, // 分享图标
 						success: function () { 
 							// 用户确认分享后执行的回调函数
+							alert('分享成功！');
 							Share_back();
 						},
 						cancel: function () { 
 							// 用户取消分享后执行的回调函数
+							alert('分享失败！');
 						}
 					});
 					// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
@@ -142,14 +144,17 @@ $(function(){
 		var imgUrl = $(this).attr('imgUrl') || "";
 		if(!isWeiXin()){
 			$('.sbox_tips').fadeIn();
+			alert('不进入分享！');
 		}else{
 			$('.sbox_tips').fadeIn();
 			share_weixin(aUrl,aTitle,imgUrl);
+			alert('进入分享！');
 		}
 	}
 	
 	/* 微信分享成功后 */
 	function Share_back(){
+		alert('分享回调！');
 		var url = Base.serverUrl + "activity/shareActivityCallBack";
 		var condi = {};
 		condi.login_token = g.login_token;
