c1175f3e40643e881235b4a8e9b02ecf5ccfa4da
xiaolibier <xiaolibier@126.com>
2016-08-01 10:40:56 +0800

    1

--- designerWap/res/js/getMore.js
+++ designerWap/res/js/getMore.js
@@ -80,7 +80,7 @@ $(function(){
 			var aUrl = g.aUrl || "";
 			var imgUrl = g.imgUrl || "";
 		}
-		if(!isWeiXin()){
+		if(isWeiXin()){
 			$('.sbox_tips').fadeIn();
 		}else{
 			share_weixin(aUrl,aTitle,imgUrl);
@@ -104,7 +104,7 @@ $(function(){
 				var signature = d.signature || "";
 				var nonceStr = d.nonceStr || "";
 				var timestamp = d.timestamp || "";
-				
+				alert(appId+"+"+signature+"+"+nonceStr+"+"+timestamp);
 				wx.config({
 					debug: '', // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 					appId: appId, // 必填，公众号的唯一标识
@@ -119,10 +119,12 @@ $(function(){
 					imgUrl: imgUrl, // 分享图标
 					success: function () { 
 						// 用户确认分享后执行的回调函数
+						alert('分享成功');
 						Share_back();
 					},
 					cancel: function () { 
 						// 用户取消分享后执行的回调函数
+						alert('分享失败');
 					}
 				});
 
