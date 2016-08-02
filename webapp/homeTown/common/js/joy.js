72d8ac130dd2e7224ae6f5980912364680183df9
xiaolibier <xiaolibier@126.com>
2016-08-02 16:03:06 +0800

    1121

--- webapp/homeTown/common/js/joy.js
+++ webapp/homeTown/common/js/joy.js
@@ -1,45 +1,46 @@
-$(function(){
+$(document).ready(function(){
 	var g = {};
+	g.page_id = Utils.getQueryString("page") || "";
 	set_video();
 	g.zan = 0;//点赞数
 	g.fangwen = 0;
-	var page_id = Utils.getQueryString("page") || "";
 	g.channel = '76543210';
 	read_database();
 	$('.mod-nav.no-icon').bind('click',write_zan);
 	
 	/* 配置视频参数 */
 	function set_video(){
+		
 		var title = [],poster = [],src = [];
-		title[0] = '这是个秀长腿的季节悠着点';
-		title[1] = '这就是这个季节，说多是泪';
-		title[2] = '公交车上遇到这样的女乘客';
+		title[0] = '乞丐也难当 太逗全场哄笑';//标题
+		title[1] = '美女这身材比例 实属罕见';
+		title[2] = '取了这老婆 大哥心里苦啊';
 		title[3] = '';
 		title[4] = '';
 		title[5] = '';
 		title[6] = '';
 		title[7] = '';
-		poster[0] = 'http://shp.qpic.cn/qqvideo_ori/0/q030330lgha_496_280/0';
-		poster[1] = 'http://shp.qpic.cn/qqvideo_ori/0/h0303y6e36l_496_280/0';
-		poster[2] = 'http://shp.qpic.cn/qqvideo_ori/0/y0303mdvdm8_496_280/0';
+		poster[0] = 'http://shp.qpic.cn/qqvideo_ori/0/g0315ji2682_496_280/0';//背景图片地址
+		poster[1] = 'http://shp.qpic.cn/qqvideo_ori/0/h0314l90ria_496_280/0';
+		poster[2] = 'http://shp.qpic.cn/qqvideo_ori/0/i031530k1y6_496_280/0';
 		poster[3] = '';
 		poster[4] = '';
 		poster[5] = '';
 		poster[6] = '';
 		poster[7] = '';
-		src[0] = 'http://111.202.85.153/vhot2.qqvideo.tc.qq.com/q030330lgha.m701.mp4?vkey=6F7AA6352A5780040DA366FF27D7589113F855EDB97572CC303F75A456EF12024F9FAA0ABE8275209FD1F938C4BC9A4872B6245B4A6A465C54551756BA17FB9C85C61CDDCB3002FCD87E27624A6059498D31F2256B9716A2&br=29&platform=2&fmt=auto&level=0&sdtfrom=v3010';
-		src[1] = 'http://111.202.98.150/vhot2.qqvideo.tc.qq.com/h0303y6e36l.m701.mp4?vkey=90E56A2F6CC176533CDACB39AAD1FF1AF80A38C45445FFBFEB1B4FEF997B378F0B0769EF42F6AD403382780C141731B6BF3711EF1B73168AEF0A1B38E00DCEEAE82D4823D1841212226B7F90F94EC95B5C2C9A76AFA9AC8E&br=28&platform=2&fmt=auto&level=0&sdtfrom=v3010';
-		src[2] = 'http://123.125.86.25/vhot2.qqvideo.tc.qq.com/y0303mdvdm8.m701.mp4?vkey=CE86346C9902BD514D2716C76E56F2DDB2CAF077E545E576939C5705B50BC5E0F64DDFE8745552991E91F014399FF75BFC898CC9C57666B22CE1542FE3627A7613A79C8A89271422898F06893F52094591BA4A357CE4EB21&br=29&platform=2&fmt=auto&level=0&sdtfrom=v3010';
+		//视频地址
+		src[0] = 'http://111.202.98.148/vhot2.qqvideo.tc.qq.com/g0315ji2682.m701.mp4?vkey=E49C6FBBAD40A17ED476436902B06AC5E3EFBC62BFE5A05A9412558CA1F370D3FA042238E990AE60070B8EE856AA53820C571C614C2E7062715DC6B97302830771320CC16E732DA1BE9416B0CCF31B5543DBF3AAFB40D7D1&br=29&platform=2&fmt=auto&level=0&sdtfrom=v3010';
+		src[1] = 'http://123.125.110.147/vhot2.qqvideo.tc.qq.com/h0314l90ria.m701.mp4?vkey=0592D3E02625DC96456CED2BE382265693B6473C60C7B808CC06C93FCDA37ED2851E57D4D4F70DD95AB3ADDA0C36B03542CDA53FC6128B81DE2F0A74FE30F1E17FD8D128E8E04ACFE2CBDD26C889113E29FB30908B073248&br=28&platform=2&fmt=auto&level=0&sdtfrom=v3010';
+		src[2] = 'http://111.202.85.147/vhot2.qqvideo.tc.qq.com/i031530k1y6.m701.mp4?vkey=24646434DCFD022DDEFF8D69FE4240910405CE0BBBDC916B99ACD6C9FA89E2DE805A29B87E5474094913224DDF28D687153238FC22DF59D371D3F4FA5FD434853B38E2FF8C1223104E6D846A37F975BA251F6CF52EC519FE&br=29&platform=2&fmt=auto&level=0&sdtfrom=v3010';
 		src[3] = '';
 		src[4] = '';
 		src[5] = '';
 		src[6] = '';
 		src[7] = '';
-		$('#playVideo').attr('poster',poster[page_id]);
-		//$('.zhuan_img img').attr('src',poster[page_id]);
-		$('#playVideo source').attr('src',src[page_id]);
-		$('.title_title,.j_title').html(title[page_id]);
-		
+		$('#playVideo').attr('poster',poster[g.page_id]);
+		//$('.zhuan_img img').attr('src',poster[g.page_id]);
+		$('#playVideo').attr('src',src[g.page_id]);
+		$('.title_title,.j_title').html(title[g.page_id]);
 	}
 	
 	/* 写入点赞 */
