c8e9600df66b53c58bdd1e23062d1ba8371971a4
xiaolibier <xiaolibier@126.com>
2016-08-02 10:52:25 +0800

    修改展业前端文案及bug

--- designerWap/res/js/customerStatus.js
+++ designerWap/res/js/customerStatus.js
@@ -28,7 +28,7 @@ $(function(){
 			case '10050301': $(".left_line2").addClass('active').find('.text').html('风控初审中');   break;
 			case '10050302': $(".left_line2").addClass('active').find('.text').html('风控复审中'); break;
 			case '10050303': $(".left_line2").addClass('active').find('.text').html('风控终审中'); break;
-			case '100502': $(".left_line2").addClass('active').find('.text').html('风控终审中'); break;
+			case '100502': $(".left_line2").addClass('active').find('.text').html('商家审核中'); break;
 			case '100515': $(".left_line3").addClass('active').find('.text').html('待确认'); break;
 			case '100505': $(".left_line3").addClass('active').find('.text').html('待缴费'); break;
 			case '100506': $(".left_line4").addClass('active').find('.text').html('待放款'); break;
