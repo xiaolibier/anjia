6c31d787e56335b38adee8a8d723d47d4012e46a
xiaolibier <xiaolibier@126.com>
2016-08-08 09:28:16 +0800

    23423

--- webapp/homeTown/common/js/joy.js
+++ webapp/homeTown/common/js/joy.js
@@ -16,9 +16,9 @@ $(document).ready(function(){
 		title[1] = '美女的回答同伴都听不下去';
 		title[2] = '大师最后也是自作自受哈哈';
 		title[3] = '街上遇高手 耍的太厉害';
-		title[4] = '';
-		title[5] = '';
-		title[6] = '';
+		title[4] = '开门遇到女神问这样的问题';
+		title[5] = '女友呆萌的可爱 笑喷了';
+		title[6] = '男友这么体贴最后女友气疯';
 		title[8] = '';
 		title[9] = '';
 		title[10] = '';
@@ -29,9 +29,9 @@ $(document).ready(function(){
 		poster[1] = 'http://shp.qpic.cn/qqvideo_ori/0/z0315w9gmc5_496_280/0';
 		poster[2] = 'http://shp.qpic.cn/qqvideo_ori/0/x0316u3gby5_496_280/0';
 		poster[3] = 'http://shp.qpic.cn/qqvideo_ori/0/u03168oi7vl_496_280/0';
-		poster[4] = '';
-		poster[5] = '';
-		poster[6] = '';
+		poster[4] = 'http://shp.qpic.cn/qqvideo_ori/0/i03172u2tsu_496_280/0';
+		poster[5] = 'http://shp.qpic.cn/qqvideo_ori/0/l0317mebf4n_496_280/0';
+		poster[6] = 'http://shp.qpic.cn/qqvideo_ori/0/w0317jw4m4r_496_280/0';
 		poster[7] = '';
 		poster[8] = '';
 		poster[9] = '';
@@ -44,9 +44,9 @@ $(document).ready(function(){
 		src[1] = 'http://123.125.110.142/vhot2.qqvideo.tc.qq.com/z0315w9gmc5.m701.mp4?vkey=5F0520FCA34B51C19188D9B16FDC74E801EBA87CB050E34D26FC1AAE6FF181F78728F07D40A0899032B994F2160AEBF58E1105EB5B1B29F0214BC85BE3D795E24499FDC643AE5A6584A5C120A91C33429E6D1A8D2EA39516&br=29&platform=2&fmt=auto&level=0&sdtfrom=v3010';
 		src[2] = 'http://123.125.86.30/vhot2.qqvideo.tc.qq.com/x0316u3gby5.m701.mp4?vkey=1110F1BB4B900A90881C995103A80145281B5E14CF1134CF1253BE1C8E951E4C7653256738EB0F51FF8A8246F5F84774FC6ABA269A7C883925B4F9D374EAA47EA751D626375AFA2F56BB445891CE4F0B2E20DD9C885AEA22&br=29&platform=2&fmt=auto&level=0&sdtfrom=v3010';
 		src[3] = 'http://111.202.98.150/vhot2.qqvideo.tc.qq.com/u03168oi7vl.m701.mp4?vkey=86F5C1393AE064EBDDDA2DA39C9FA8AE90EDC0F6319C08F9F1B7C673499FE34BCA6D9ABF402CD101C1BCECF7DADA136298E48E763DE662479184D80221774F78FC1FE9A98CB947A99596BAB05841735BD541D2198717B24B&br=29&platform=2&fmt=auto&level=0&sdtfrom=v3010';
-		src[4] = '';
-		src[5] = '';
-		src[6] = '';
+		src[4] = 'http://111.202.85.143/vhot2.qqvideo.tc.qq.com/i03172u2tsu.m701.mp4?vkey=A45372B7C1968A37DF8E5F5A2ED81691F2C6D53DBC00428AD595818BD918FE9CA17E2E69F2A4E655EB7CD20540765ED1FE5E58299929E441E54877D79668F74A6F0E3C913FA38F807B0995F4DB8B7895CADCB77869FABEAD&br=29&platform=2&fmt=auto&level=0&sdtfrom=v3010';
+		src[5] = 'http://111.202.85.147/vhot2.qqvideo.tc.qq.com/l0317mebf4n.m701.mp4?vkey=8889F3CA0FD0C92EBA6AAC6535AF7E56AE2D669E982C08D18C7F339C919625F891403287A10C157B5049157972347BE398F1561FF84A35A524156374DDCC27F836AC3935E336A29CE7B98BE1A224B381F76EDC4A9ED1A7D6&br=29&platform=2&fmt=auto&level=0&sdtfrom=v3010';
+		src[6] = 'http://123.125.110.142/vhot2.qqvideo.tc.qq.com/w0317jw4m4r.m701.mp4?vkey=EFB1D20A4DB20096D14845508654A455CC1C812AEFF34817697A51002B53795BCEFAC59E8E90D4F89B101A6F471CFD54F1E89FE757D7060C459913D1CCC732B14E376496D9E589BD66B77311812B5DFB89D2914E66C80D94&br=29&platform=2&fmt=auto&level=0&sdtfrom=v3010';
 		src[7] = '';
 		src[8] = '';
 		src[9] = '';
