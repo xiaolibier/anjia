4337c6f43411dcf94e2397373ad650b207278766
xiaolibier <xiaolibier@126.com>
2016-09-02 16:01:49 +0800

    修改展业系统 注册跳转链接

--- designerWap/res/js/reg.js
+++ designerWap/res/js/reg.js
@@ -285,7 +285,7 @@ $(function(){
 				var status = data.success || false;
 				if(status){
 					alert('恭喜你！提交成功,请等候审核');
-					location.href = "http://mp.weixin.qq.com/s?__biz=MzIxNzA1MjkyMg==&mid=2247483653&idx=1&sn=a7a5e7b1a00bd4b9ec1f54e860005365&scene=0#wechat_redirect";
+					location.href = "http://mp.weixin.qq.com/s?__biz=MzIzMzU2NDc4Nw==&mid=100000005&idx=1&sn=ec4a31240368eb004ae900d04d1d3ccf&scene=0#wechat_redirect";
 					/* var userInfo = data.obj || "";
 					if(userInfo !== ""){
 						g.customerId = userInfo.customerId || "";
